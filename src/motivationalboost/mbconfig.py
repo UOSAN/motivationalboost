7cff7d594de2912b93717feadac333f444e71dc4
Paul Novak <pnovak@alumni.caltech.edu>
2019-09-13 10:02:30 -0700

    Read configuration from configuration file, instead of hard-coding it

---
+++ src/motivationalboost/mbconfig.py
@@ -0,0 +1,13 @@
+import json
+
+
+class MBConfig:
+    """
+    Class to read configuration for motivational boost from the environment
+    """
+    def __init__(self):
+        with open('config.json') as f:
+            configuration = json.load(f)
+            self.qualtrics_api_token= configuration['qualtrics_api_token']
+            self.apptoto_api_token = configuration['apptoto_api_token']
+            self.apptoto_user = configuration['apptoto_user']
