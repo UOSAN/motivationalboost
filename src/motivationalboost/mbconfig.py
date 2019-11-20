526d629a642f7038a51b52c02548a916fd147e18
Paul Novak <pnovak@alumni.caltech.edu>
2019-11-20 10:20:47 -0800

    Whitespace change

--- src/motivationalboost/mbconfig.py
+++ src/motivationalboost/mbconfig.py
@@ -8,6 +8,6 @@ class MBConfig:
     def __init__(self):
         with open('config.json') as f:
             configuration = json.load(f)
-            self.qualtrics_api_token= configuration['qualtrics_api_token']
+            self.qualtrics_api_token = configuration['qualtrics_api_token']
             self.apptoto_api_token = configuration['apptoto_api_token']
             self.apptoto_user = configuration['apptoto_user']
