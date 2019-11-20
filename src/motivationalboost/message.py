718c45d21ff62fbd91188c026e1452c360c3ec58
Paul Novak <pnovak@alumni.caltech.edu>
2019-11-20 10:05:09 -0800

    Implement stubbed-out Message methods, add unit tests
    Implement Message methods to get the time to send a message, message content
    and title. Add method to set the placeholders to use to populate the
    message. Add unit tests for these methods.

--- src/motivationalboost/message.py
+++ src/motivationalboost/message.py
@@ -1,6 +1,9 @@
 import re
+from datetime import datetime, timedelta
 from string import Template
-from typing import List
+from typing import List, Mapping
+
+from src.motivationalboost.parse_date_time import parse_datetime_string
 
 
 class Message:
@@ -10,6 +13,7 @@ class Message:
         self._start_date_template = Template(start_date)
         self._start_time_template = Template(start_time)
         self._title_template = Template(title)
+        self._placeholders = None
 
     def get_placeholders(self) -> List[str]:
         """Return a set of all the placeholders in the message"""
@@ -25,14 +29,31 @@ class Message:
         p = re.compile(pattern, re.IGNORECASE | re.VERBOSE)
         return p.findall(self._content_template.template)
 
+    def set_placeholders(self, placeholders: Mapping[str, str]):
+        self._placeholders = placeholders
+
     def get_content(self) -> str:
         """Return the substituted template content string"""
-        pass
+        return self._content_template.substitute(self._placeholders)
 
     def get_title(self) -> str:
         """Return the substituted template title string"""
-        pass
+        return self._title_template.substitute(self._placeholders)
 
-    def get_start_date_time(self) -> str:
-        """Return the substituted date time string"""
-        pass
+    def get_message_time(self) -> datetime:
+        """
+        Get the date time that the message should be sent
+        :return: a datetime
+        """
+        if self._schedule_template == '' or self._schedule_template == 'now':
+            # TODO: Perhaps delay the "now" response by a few minutes to make sure it can get scheduled and sent
+            return datetime.now()
+        else:
+            start_date_time = f'{self._start_date_template.substitute(self._placeholders)} ' \
+                              f'{self._start_time_template.substitute(self._placeholders)}'
+            start = parse_datetime_string(start_date_time)
+
+            if self._schedule_template.endswith('h'):
+                start = start + timedelta(hours=int(self._schedule_template[:-1]))
+
+            return start
--- tests/message/message_test.py
+++ tests/message/message_test.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from src.motivationalboost.message import Message
 
 
@@ -18,3 +20,71 @@ class TestMessage:
         m = Message(content=content, schedule='', start_date='', start_time='', title='')
 
         assert len(m.get_placeholders()) == 1
+
+    def test_get_content(self):
+        content = 'Test ${PH01} string ${PH02}'
+        placeholders = {'PH01': 'failure', 'PH02': 'cheese'}
+
+        m = Message(content=content, schedule='', start_date='', start_time='', title='')
+        m.set_placeholders(placeholders=placeholders)
+
+        actual = m.get_content()
+
+        assert actual == 'Test failure string cheese'
+        # Verify that title is an empty string because it has no placeholders
+        assert m.get_title() == ''
+
+    def test_get_title(self):
+        title = 'Test ${PH01} string ${PH02}'
+        placeholders = {'PH01': 'failure', 'PH02': 'cheese'}
+
+        m = Message(content='', schedule='', start_date='', start_time='', title=title)
+        m.set_placeholders(placeholders=placeholders)
+
+        actual = m.get_title()
+
+        assert actual == 'Test failure string cheese'
+
+        # Verify that content is an empty string because it has no placeholders
+        assert m.get_content() == ''
+
+    def test_get_message_time_relative_time(self):
+        start_date = '${PH01}'
+        start_time = '${PH02}'
+        schedule = '-2h'
+        placeholders = {'PH01': '08-04-2019', 'PH02': '4:30pm'}
+
+        m = Message(content='', schedule=schedule, start_date=start_date, start_time=start_time, title='')
+        m.set_placeholders(placeholders=placeholders)
+
+        # Expected time is two hours before start_time, because of the relative offset from schedule
+        expected = datetime(year=2019, month=8, day=4, hour=14, minute=30)
+
+        assert m.get_message_time() == expected
+
+    def test_get_message_time_now(self):
+        start_date = '${PH01}'
+        start_time = '${PH02}'
+        schedule = 'now'
+        placeholders = {'PH01': '08-04-2019', 'PH02': '4:30pm'}
+
+        m = Message(content='', schedule=schedule, start_date=start_date, start_time=start_time, title='')
+        m.set_placeholders(placeholders=placeholders)
+
+        # Verify that message_time is essentially now.
+        expected = datetime.now()
+
+        assert (m.get_message_time() - expected).microseconds < 1000
+
+    def test_get_message_time_missing_schedule(self):
+        start_date = '${PH01}'
+        start_time = '${PH02}'
+        schedule = ''
+        placeholders = {'PH01': '08-04-2019', 'PH02': '4:30pm'}
+
+        m = Message(content='', schedule=schedule, start_date=start_date, start_time=start_time, title='')
+        m.set_placeholders(placeholders=placeholders)
+
+        # Verify that message_time is essentially now.
+        expected = datetime.now()
+        assert (m.get_message_time() - expected).microseconds < 1000
