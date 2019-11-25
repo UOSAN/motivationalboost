ad761b0cf56b5c25a0fac2d65082a1b058aeaf56
Paul Novak <pnovak@alumni.caltech.edu>
2019-11-25 09:15:36 -0800

    Add timezone to parsed datetime strings
    Add the fixed, hardcoded timezone 'America/Los_Angeles' to the datetime
    objects that result from parsing strings.

--- motivationalboost/parse_date_time.py
+++ motivationalboost/parse_date_time.py
@@ -1,4 +1,5 @@
-import datetime
+from datetime import datetime
+import dateutil
 
 
 def parse_datetime_string(datetime_string: str) -> datetime:
@@ -7,8 +8,11 @@ def parse_datetime_string(datetime_string: str) -> datetime:
     '%m-%d-%Y %I:%M%p' (i.e., '08-04-2019 2:30pm') or
     '%m-%d-%Y %I%p' (i.e., '08-04-2019 2pm')
     """
+    tz = dateutil.tz.gettz('America/Los_Angeles')
     try:
-        return datetime.datetime.strptime(datetime_string, '%m-%d-%Y %I:%M%p')
+        temp = datetime.strptime(datetime_string, '%m-%d-%Y %I:%M%p')
+        return temp.replace(tzinfo=tz)
     except ValueError:
-        return datetime.datetime.strptime(datetime_string, '%m-%d-%Y %I%p')
+        temp = datetime.strptime(datetime_string, '%m-%d-%Y %I%p')
+        return temp.replace(tzinfo=tz)
 
