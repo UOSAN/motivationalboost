7c34ac761572bbabbb3d199f0815b169d99bc125
Paul Novak <pnovak@alumni.caltech.edu>
2019-12-06 10:25:32 -0800

    Change default path to finding configuration

--- motivationalboost/mbconfig.py
+++ motivationalboost/mbconfig.py
@@ -1,4 +1,5 @@
 import json
+from pathlib import Path
 import os
 
 
@@ -6,9 +7,9 @@ class MBConfig:
     """
     Class to read configuration for motivational boost from the environment
     """
-    def __init__(self, path: str = None):
+    def __init__(self, path: Path = Path.cwd() / 'instance'):
         if path:
-            with open(os.path.join(path, 'config.json')) as f:
+            with open(str(path / 'config.json')) as f:
                 configuration = json.load(f)
                 self.qualtrics_api_token = configuration['qualtrics_api_token']
                 self.apptoto_api_token = configuration['apptoto_api_token']
