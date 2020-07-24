586e0dce255221b2f7a7eb4ea68dda982c2cd1ac
Paul Novak <pnovak@alumni.caltech.edu>
2020-07-24 13:54:23 -0700

    Support multiple groups of message templates
    Support multiple groups of message templates, using the survey_id
    of the response to select which group of messages will be sent.

--- tests/message/message_container_test.py
+++ tests/message/message_container_test.py
@@ -29,8 +29,20 @@ class TestMessageContainer:
         shutil.copyfile((shared_datadir / 'valid.json'), template_path / 'message_template.json')
 
         message_container = MessageContainer(template_path=template_path)
+        messages = message_container.get_messages(survey_id='SV_1234')
 
-        assert len(message_container) == 2
+        assert len(messages) == 2
+
+    def test_container_invalid_survey_id(self, tmp_path, shared_datadir):
+        # Create a valid file in test-controlled path, and verify that the two messages are read
+        template_path = tmp_path / 'templates'
+        template_path.mkdir()
+        shutil.copyfile((shared_datadir / 'valid.json'), template_path / 'message_template.json')
+
+        message_container = MessageContainer(template_path=template_path)
+        messages = message_container.get_messages(survey_id='invalid_survey_id')
+
+        assert messages is None
 
     def test_get_placeholders(self, tmp_path, shared_datadir):
         # Create a valid file in test-controlled path, and verify the set of placeholders returned
@@ -40,8 +52,24 @@ class TestMessageContainer:
 
         message_container = MessageContainer(template_path=template_path)
 
-        placeholders = message_container.get_placeholders()
+        placeholders = message_container.get_placeholders(survey_id='SV_1234')
         expected_placeholders = {'QID03', 'QID41', 'QID42', 'QID47', 'QID48', 'QID30', 'QID23'}
 
         assert len(placeholders) == 7
         assert len(placeholders - expected_placeholders) == 0
+
+    def test_container_with_multiple_groups_of_message_templates(self, tmp_path, shared_datadir):
+        # Create a valid file in test-controlled path, and verify that the there are two
+        # groups of message templates with different numbers of message templates
+        template_path = tmp_path / 'templates'
+        template_path.mkdir()
+        shutil.copyfile((shared_datadir / 'multiple.json'), template_path / 'message_template.json')
+
+        message_container = MessageContainer(template_path=template_path)
+        messages = message_container.get_messages(survey_id='SV_1234')
+
+        assert len(messages) == 2
+
+        messages = message_container.get_messages(survey_id='SV_abcd')
+
+        assert len(messages) == 3
