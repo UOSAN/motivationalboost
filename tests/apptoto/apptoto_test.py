53557ad85c3340f5db346a591111f788a13dab4d
Paul Novak <pnovak@alumni.caltech.edu>
2020-07-30 13:49:22 -0700

    Verify captured logging output in tests

--- tests/apptoto/apptoto_test.py
+++ tests/apptoto/apptoto_test.py
@@ -1,3 +1,4 @@
+import logging
 from datetime import datetime
 
 import requests
@@ -12,33 +13,39 @@ _user = 'test_user'
 
 
 class TestApptoto:
-    def test_post_events_success(self, requests_mock, capsys):
-        # Arrange
-        a = Apptoto(api_token=_api_token, user=_user)
-        url = f'{_endpoint}/events'
-        now = datetime.now(tz=tz.gettz('America/Los_Angeles'))
-        events = [ApptotoEvent(calendar='', title='', start_time=now, end_time=now, content='', participants=[])]
-        requests_mock.post(url=url,
-                           status_code=requests.codes.ok)
+    def test_post_events_success(self, requests_mock, caplog):
+        with caplog.at_level(logging.INFO):
+            # Arrange
+            a = Apptoto(api_token=_api_token, user=_user)
+            url = f'{_endpoint}/events'
+            now = datetime.now(tz=tz.gettz('America/Los_Angeles'))
+            events = [ApptotoEvent(calendar='', title='', start_time=now, end_time=now, content='', participants=[])]
+            requests_mock.post(url=url,
+                               status_code=requests.codes.ok)
 
-        # Act
-        post = a.post_events(events)
+            # Act
+            post = a.post_events(events)
 
-        # Assert
-        assert post
+            # Assert
+            assert post
+            assert 'Posted events to apptoto' in caplog.text
 
-    def test_post_events_failure(self, requests_mock, capsys):
-        # Verify no output in the case of failing to post events.
-        # Arrange
-        a = Apptoto(api_token=_api_token, user=_user)
-        url = f'{_endpoint}/events'
-        now = datetime.now(tz=tz.gettz('America/Los_Angeles'))
-        events = [ApptotoEvent(calendar='', title='', start_time=now, end_time=now, content='', participants=[])]
-        requests_mock.post(url=url,
-                           status_code=requests.codes.bad_request)
+    def test_post_events_failure(self, requests_mock, caplog):
+        with caplog.at_level(logging.INFO):
+            # Verify log output in the case of failing to post events.
+            # Arrange
+            a = Apptoto(api_token=_api_token, user=_user)
+            url = f'{_endpoint}/events'
+            now = datetime.now(tz=tz.gettz('America/Los_Angeles'))
+            events = [ApptotoEvent(calendar='', title='', start_time=now, end_time=now, content='', participants=[])]
+            content = b'binary content'
+            requests_mock.post(url=url,
+                               status_code=requests.codes.bad_request,
+                               content=content)
 
-        # Act
-        post = a.post_events(events)
+            # Act
+            post = a.post_events(events)
 
-        # Assert
-        assert not post
+            # Assert
+            assert not post
+            assert 'Failed to post' in caplog.text and str(content) in caplog.text
